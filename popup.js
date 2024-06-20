2e52b7414d28893430faca2b281c2f8c7be3f166
InformatiquePro <161227509+InformatiquePro@users.noreply.github.com>
2024-06-20 10:09:38 +0200

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,11 @@
+document.getElementById('generateButton').addEventListener('click', generatePassword);
+
+function generatePassword() {
+  const length = 16;
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+  let password = "";
+  for (let i = 0, n = charset.length; i < length; ++i) {
+    password += charset.charAt(Math.floor(Math.random() * n));
+  }
+  document.getElementById('passwordOutput').textContent = password;
+}
